749e5ae5097e167e3ed631191ea5dc65cdcb4319
bhavyasri-22 <bhavyasrithota.08@gmail.com>
2025-12-15 19:34:02 +0530

    Update manager.js

--- CloudFunc/container-manager/manager.js
+++ CloudFunc/container-manager/manager.js
@@ -5,25 +5,20 @@ const app = express();
 const docker = new Docker(); 
 const PORT = 4000;
 
-// Test endpoint
 app.get("/spawnTestContainer", async (req, res) => {
   let container;
 
   try {
-    // 1. Create container
     container = await docker.createContainer({
       Image: "alpine",
       Cmd: ["echo", "Hello from CloudFunc"],
       Tty: false
     });
 
-    // 2. Start container
     await container.start();
 
-    // 3. Wait till command finishes
     await container.wait();
 
-    // 4. Remove container
     await container.remove();
 
     res.json({
